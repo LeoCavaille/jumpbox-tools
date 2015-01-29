fe36863b86572b72880f3ab74833ff119c8369b4
Léo Cavaillé <leo@datadoghq.com>
2015-01-29 21:43:30 +0000

    Add RC file and resource alias

--- links.sh
+++ links.sh
@@ -1,14 +1,14 @@
+#!/
+CDIR=~/.jumpbox-tools
+
 rmlink(){
     rm -f $2
-    rm -f $CDIR/$1 $2
+    ln -s $CDIR/$1 $2
 }
 
 # zsh
 rmlink zsh/env ~/.zshenv
 rmlink zsh/rc ~/.zshrc
-
-source ~/.zshenv
-source ~/.zshrc
 
 # tools
 rmlink bin ~/.bin
