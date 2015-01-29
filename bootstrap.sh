9cfb16f1fdb69c5b85669e9cd8f91aef5dfd04ed
Leo Cavaille <leo@ip-10-179-145-66.ec2.internal>
2015-01-29 21:47:32 +0000

    Remove myself

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,9 @@
 # GROS MÉNAGE
 cd ~
 # Don't delete .ssh otherwise you cannot get in!!
-rm -rf .bash* .bin* .cache* .chef* .config* .lesshst .oh-my-zsh .profile .ssh/known_hosts .vim* .zcomp* .zsh*
+rm -rf .bash* .bin* .cache* .chef* .config* .gitconfig .lesshst .oh-my-zsh .profile .ssh/known_hosts .vim* .zcomp* .zsh*
+# In case we are re-running it
+rm -rf .jumpbox-tools
 
 # Clone ourselves
 git clone git@github.com:LeoCavaille/jumpbox-tools ~/.jumpbox-tools
