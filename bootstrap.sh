0b2e310e51b55ca619449d467415c7a461b9e6ed
curtis.quintal <curtis.quintal@COMP11583.local>
2019-08-23 14:53:13 -0400

    Adds more logic to check if files exist before attempting copy. Updates logging statements.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,22 @@
 cd ~
 
 # Backup existing dotfiles
+printf "Creating backups of your dotfiles at ~/.jumpbox_dotfile_backups.\nRunning this again will replace these backups with the files this generates.\n\n"
 mkdir .jumpbox_dotfile_backups
-cp {.bash*,.bin*,.cache*,.chef*,.config*,.gitconfig,.lesshst,.oh-my-zsh,.profile,.ssh/known_hosts,.vim*,.zcomp*,.zsh*} .jumpbox_dotfile_backups/
-echo "This created backups of your dotfiles at ~/.jumpbox_dotfile_backups. Running this again will replace these backups."
+for fname in .bash* .bin* .cache* .chef* .config* .gitconfig .lesshst .oh-my-zsh .profile .ssh/known_hosts .vim* .zcomp* .zsh*
+do
+  if [ -f $fname ]; then
+    cp $fname .jumpbox_dotfile_backups
+    echo "~/$fname copied to ~/.jumpbox_dotfile_backups/$fname"
+  fi
+  if [ -d $fname ]; then
+    cp -r $fname .jumpbox_dotfile_backups
+    echo "~/$fname copied to ~/.jumpbox_dotfile_backups/$fname"
+  fi
+done
 
 # Don't delete .ssh otherwise you cannot get in!!
+echo "Removing/replacing dotfiles which have been backed up."
 rm -rf .bash* .bin* .cache* .chef* .config* .gitconfig .lesshst .oh-my-zsh .profile .ssh/known_hosts .vim* .zcomp* .zsh*
 # In case we are re-running it
 rm -rf .jumpbox-tools
